fa4a57bce81474d3ffff3ce5e1f60fe1fec51773
Paulina <paulina.a.wojtal@gmail.com>
2021-04-13 12:11:24 +0200

    better Vehicles added to table

--- src/main/java/wojtal/paulina/Main.java
+++ src/main/java/wojtal/paulina/Main.java
@@ -8,19 +8,13 @@ public class Main
 {
     public static void main( String[] args )
     {
-        Vehicle[] vehicles = new Vehicle[5];
-
-        Vehicle v1 = new Truck("BigTruck");
-        Vehicle v2 = new RaceCar("Fastcar");
-        Vehicle v3 = new Car("JustACar");
-        Vehicle v4 = new Plane("Boeing");
-        Vehicle v5 = new Ship("Ship");
-
-        vehicles[0] = v1;
-        vehicles[1] = v2;
-        vehicles[2] = v3;
-        vehicles[3] = v4;
-        vehicles[4] = v5;
+        Vehicle[] vehicles = {
+                new Truck("BigTruck"),
+                new RaceCar("Fastcar"),
+                new Car("JustACar"),
+                new Plane("Boeing"),
+                new Ship("Ship")
+        };
 
         for (Vehicle v : vehicles) {
             System.out.println(v.toString());
